61805eea2e6c05725e24bb3c04c062252af6f341
5605104046 <560510404@live4.utcc.ac.th>
2015-11-21 19:30:58 +0700

    edit data

--- insert_data.sql
+++ insert_data.sql
@@ -6,7 +6,7 @@ USE 5605104046db;
 
 SET AUTOCOMMIT=0;
 INSERT INTO `5605104046db`.`Course` (`Co_id`, `Co_credit`, `Co_hours`) VALUES ('1001', '3', '30');
-INSERT INTO `5605104046db`.`student` (`Std_id`, `Std_pass`, `Std_age`, `Std_address`) VALUES ('5605104046', '1234', '20', '44/44 utcc');
+INSERT INTO `5605104046db`.`student` (`Std_id`, `Std_pass`, `Std_age`, `Std_address`) VALUES ('565104046', '1234', '20', '44/44 utcc');
 INSERT INTO `5605104046db`.`officer` (`Off_id`, `Off_name`, `Off_pass`, `Off_age`, `Off_address`) VALUES ('12345678', 'ruk', '1234', '32', '55/66');
 INSERT INTO `5605104046db`.`registers` (`Reg_id`, `Credit_total`, `Std_id`, `Off_id`) VALUES ('12345', '138', '560510401', '12345678');
 INSERT INTO `5605104046db`.`register_details` (`Reg_id`, `RD_Room`, `Co_id`) VALUES ('12345', '21503', '1001');
